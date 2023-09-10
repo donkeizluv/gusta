3031150955ef120a736326904f99d9e155b2a410
Luu Hong <5866540+donkeizluv@users.noreply.github.com>
2023-09-10 16:38:20 +0700

    add read config from exe

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,5 @@
-use anyhow::Result;
-use std::{env, fs};
+use anyhow::{Error, Result};
+use std::{env, fs, path::PathBuf};
 
 use serde::Deserialize;
 
@@ -13,13 +13,29 @@ const CONFIG_FILENAME: &str = "Config.toml";
 
 impl Config {
     pub fn read_env() -> Result<Self> {
-        let env = env::current_dir()?.join(CONFIG_FILENAME);
-        let setting_str = match fs::read_to_string(env) {
-            Ok(s) => s,
-            Err(_) => panic!("missing config file"),
-        };
-        let setting: Self = toml::from_str(&setting_str)?;
+        let from_pwd = env::current_dir()?.join(CONFIG_FILENAME);
+        let from_exe = Self::exe_dir()?;
 
-        Ok(setting)
+        let setting_str = match Self::opt_read(from_pwd) {
+            Some(s) => Some(s),
+            None => Self::opt_read(from_exe),
+        };
+
+        match setting_str {
+            Some(s) => Ok(toml::from_str(&s)?),
+            None => Err(Error::msg("unable to find config file")),
+        }
+    }
+    fn opt_read(path: PathBuf) -> Option<String> {
+        match fs::read_to_string(path) {
+            Ok(s) => Some(s),
+            Err(_) => None,
+        }
+    }
+
+    fn exe_dir() -> Result<PathBuf> {
+        let mut exe = env::current_exe()?;
+        exe.pop();
+        Ok(exe)
     }
 }
